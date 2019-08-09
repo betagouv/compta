3fc512268983d79ba41eacae6715f8ea99927a8f
Thomas Guillet <guillet.thomas@gmail.com>
2019-08-09 14:53:10 +0200

    Simplifie l'authentification Google

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Outil de facilitation du suivi budgétaire et comptable
 
+
+Ce dépôt contient deux parties
+
+## Le front
+
+> En NextJS
+
+```bash
+cd front
+npm ci
+npm run dev
+python -mwebbrowser http://localhost:3000/
+````
+
+## Le back
+
+> En Python
+
+Qui repose sur l'API Google Sheet pour récupérer des données.
+Une clé d'API est nécessaire : https://console.developers.google.com
+
+```bash
+cd back
+virtualenv .venv --python=python3.6
+source .venv/bin/activate
+pip install --requirement requirements.txt --upgrade
+GOOGLE_API_KEY=VOTRE-CLE-API-GOOGLE FLASK_DEBUG=1 FLASK_APP=app.py flask run
+```
--- back/onlinesheet.py
+++ back/onlinesheet.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 import datetime
+import os
 import os.path
 import pickle
 import re
 
 import pandas as pd
-
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1pZYJvjUeMPF2oWzDOp6SC-CECcb3zmt5xq-udeEcELg'
@@ -59,29 +55,7 @@ def aggregateEJ(data):
 
 
 def getsheet():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=8081)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('sheets', 'v4', credentials=creds)
+    service = build('sheets', 'v4', developerKey=os.environ['GOOGLE_API_KEY'])
     return service.spreadsheets()
 
 

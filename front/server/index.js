09e1553231586aa4e78d565cdea9058c9c208a85
Thomas Guillet <guillet.thomas@gmail.com>
2019-08-09 15:21:39 +0200

    Ajoute express pour le SSR

---
+++ front/server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const next = require('next')
+const compression = require('compression')
+
+const port = process.env.PORT || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({dev})
+
+app.prepare().then(() => {
+  const server = express()
+
+  if (!dev) {
+    server.use(compression())
+  }
+
+  server.get('*', (req, res) => {
+    app.render(req, res, req.params[0], req.query)
+  })
+
+  server.listen(port, err => {
+    if (err) {
+      throw err
+    }
+
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
